224044bab5565ed42859dc101e0901196ea91b68
pradeep0317 <stumperprd@gmail.com>
2026-04-02 04:08:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// INPUT (website → unity)
+let inputData = {};
+
+// OUTPUT (unity → website)
+let outputData = {};
+
+app.post("/data", (req, res) => {
+  inputData = req.body;
+  res.json({ status: "ok" });
+});
+
+app.get("/data", (req, res) => {
+  res.json(inputData);
+});
+
+app.post("/output", (req, res) => {
+  outputData = req.body;
+  res.json({ status: "received" });
+});
+
+app.get("/output", (req, res) => {
+  res.json(outputData);
+});
+
+app.listen(3000, () => console.log("Server running on 3000"));
